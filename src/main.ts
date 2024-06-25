c7e877bfa6dcc8e3b792c6968beffd5393267310
ehotigas <twsandrade@gmail.com>
2024-06-25 20:19:59 -0300

    update: cors enable

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   const config = new DocumentBuilder().setTitle(
     'Remote Terminal'
   ).setDescription(
