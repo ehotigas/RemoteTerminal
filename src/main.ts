f4dc681c6d2b0f102a404f0b526428e87f3229c4
ehotigas <twsandrade@gmail.com>
2024-06-25 08:16:34 -0300

    new: project, swagger configs

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder().setTitle(
+    'Remote Terminal'
+  ).setDescription(
+    'An API to remote terminal access'
+  ).setVersion(
+    '1.0'
+  ).build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+}
+
+bootstrap();
