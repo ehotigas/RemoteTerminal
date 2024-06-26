99b4a302ed3bed3b0d05a4bd92be30a81bd54f35
ehotigas <90466869+ehotigas@users.noreply.github.com>
2024-06-26 08:36:38 -0300

    update: project port

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(5000);
+  await app.listen(5001);
 }
 
 bootstrap();
